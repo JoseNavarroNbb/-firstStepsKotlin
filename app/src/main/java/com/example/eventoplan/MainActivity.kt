10e257a879aaed2d9e4891e6fa23d7e0fe873284
José Carlos Navarro Beristain <carlos.navarro@AUDITORIAPUEBLA.GOB.MX>
2023-11-29 11:04:10 -0600

    se agregan iconos y se ajustan

--- app/src/main/java/com/example/eventoplan/MainActivity.kt
+++ app/src/main/java/com/example/eventoplan/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.eventoplan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,11 +15,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.eventoplan.ui.theme.EventoPlanTheme
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +36,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Feliz Cumple", from = "From I´m" )
+                    GreetingImage(message = getString(R.string.feliz_cumple),
+                        from = getString(R.string.signature_text) )
 
                 }
             }
@@ -57,18 +64,42 @@ fun GreetingText( message: String, from: String, modifier: Modifier = Modifier) 
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop, //the image occupies the entire screen
+            alpha = 0.5F //background image opacity
+        )
+
+
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 
 }
 
 @Preview(
-    showBackground = true
+    showBackground = false
 )
 @Composable
 fun GreetingPreview() {
     EventoPlanTheme {
-        GreetingText(message = "Feliz Cumple", from = " From I´m")
+        GreetingImage(message = "Feliz Cumple", from = " From I´m")
     }
 }
